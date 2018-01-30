904aaf31e9098c6a85e906a2f05f6587ba6c8277
GG Bone <1109113244@qq.com>
2018-01-30 20:43:14 +0800

    接口规范添加了

--- src/com/hrsys/dao/IJobDao.java
+++ src/com/hrsys/dao/IJobDao.java
@@ -1,5 +1,17 @@
 package com.hrsys.dao;
 
-public interface IJobDao {
+import java.sql.SQLException;
 
+import com.hrsys.pojo.Job;
+/**
+ * 
+ * @ClassName: IJobDao 
+ * @Description: 职位的数据访问层接口 
+ * @author GG Bone
+ * @date 2018年1月30日 下午8:15:41 
+ *
+ */
+public interface IJobDao {
+	void insert(Job job) throws SQLException;
+	void findAllJob() throws SQLException;
 }
--- src/com/hrsys/dao/INoticeDao.java
+++ src/com/hrsys/dao/INoticeDao.java
@@ -1,5 +1,34 @@
 package com.hrsys.dao;
 
+import java.sql.SQLException;
+import com.hrsys.pojo.Notice;
+/**
+ * 
+ * @ClassName: INoticeDao 
+ * @Description: 公告数据访问接口
+ * @author GG Bone
+ * @date 2018年1月30日 下午8:12:22 
+ *
+ */
 public interface INoticeDao {
-
+	/**
+	 * 
+	 * @Title: insert 
+	 * @Description: 向数据库中添加公告
+	 * @param @param notice
+	 * @param @throws SQLException  参数说明 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	void insert(Notice notice) throws SQLException;
+	
+	/**
+	 * 
+	 * @Title: findAllNotice 
+	 * @Description: 查找数据库中的所有公告 
+	 * @param @throws SQLException  参数说明 
+	 * @return void    返回类型 
+	 * @throws
+	 */
+	void findAllNotice() throws SQLException;
 }
