a8cabfc1b8f58e8e60676ebec665cccd154fa44e
GG Bone <1109113244@qq.com>
2018-02-01 09:11:21 +0800

    完成了一个过滤器

--- src/com/hrsys/contorll/UserServlet.java
+++ src/com/hrsys/contorll/UserServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class UserServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
+		
 	}
 	
 	@Override
--- src/com/hrsys/contorll/filter/CharacterFilter.java
+++ src/com/hrsys/contorll/filter/CharacterFilter.java
@@ -1,6 +1,9 @@
 package com.hrsys.contorll.filter;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,7 +11,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -23,6 +25,29 @@ public class CharacterFilter implements Filter{
 			throws IOException, ServletException {
 		HttpServletRequest request2 = (HttpServletRequest) request;
 		HttpServletResponse response2 = (HttpServletResponse) response;
+		response2.setContentType("text/html;charset=uft-8");
+		
+		HttpServletRequest req = (HttpServletRequest) Proxy.newProxyInstance(HttpServletRequest.class.getClassLoader(),
+				new Class[]{HttpServletRequest.class},
+				new InvocationHandler() {
+					
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						String sendMethod = request2.getMethod();
+						String methodName = method.getName();
+						if("getParameter".equals(methodName)) {
+							if("GET".equalsIgnoreCase(sendMethod)) {
+								String infor = (String) method.invoke(request2, args);
+								return infor;
+							}else if("POST".equalsIgnoreCase(sendMethod)) {
+								request2.setCharacterEncoding("UTF-8");
+								return method.invoke(request2, args);
+							}
+						}
+						return method.invoke(request2, args);
+					}
+				});
+		chain.doFilter(req, response2);
 	}
 	
 	@Override
